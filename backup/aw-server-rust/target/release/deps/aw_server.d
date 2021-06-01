5b25d0e02af878f40de62fd687d0e7581640fc62
minhlt9196 <minhlt9196@gmail.com>
2021-06-01 14:54:22 +0700

    Add submodules

--- backup/aw-server-rust/target/release/deps/aw_server.d
+++
@@ -1,5 +0,0 @@
-C:\Apps\Git\build-software\timebench\aw-server-rust\target\release\deps\aw_server.exe: aw-server\src/main.rs
-
-C:\Apps\Git\build-software\timebench\aw-server-rust\target\release\deps\aw_server.d: aw-server\src/main.rs
-
-aw-server\src/main.rs:
--- backup/aw-server/aw-webui/babel.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: ['lodash', '@babel/plugin-syntax-dynamic-import'],
-  comments: false,
-};
